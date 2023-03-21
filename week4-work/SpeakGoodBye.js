04eecf69e53433c14befc9271ce6b7d16eb20c27
Pawel Banasiewicz <xghoul@gmail.com>
2023-03-21 19:00:00 +0100

    cleaning

--- week4-work/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function (window){
-var speakWord = "Good Bye";
-var byeSpeaker = new Object ();
-byeSpeaker.speak = function speak(name) {
-	console.log(speakWord + " " + name)};
-window.byeSpeaker = byeSpeaker;
-})(window);
-
--- week4-work/SpeakHello.js
+++
@@ -1,7 +0,0 @@
-(function (window){
-var speakWord = "Hello";
-var helloSpeaker = new Object ();
-helloSpeaker.speak = function speak(name) {
-	console.log (speakWord + " " + name)};
-window.helloSpeaker = helloSpeaker;
-})(window);
